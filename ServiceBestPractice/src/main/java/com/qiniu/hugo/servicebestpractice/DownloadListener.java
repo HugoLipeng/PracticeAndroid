f25a5a6fcf0aa9e8f3607fef6874606a3c3331fc
HugoLipeng <1924949262@qq.com>
2019-04-28 17:54:58 +0800

    ServiceBestPractice

---
+++ ServiceBestPractice/src/main/java/com/qiniu/hugo/servicebestpractice/DownloadListener.java
@@ -0,0 +1,15 @@
+package com.qiniu.hugo.servicebestpractice;
+
+public interface DownloadListener {
+
+    void onProgress(int progress);
+
+    void onSuccess();
+
+    void onFailed();
+
+    void onPaused();
+
+    void onCanceled();
+
+}
